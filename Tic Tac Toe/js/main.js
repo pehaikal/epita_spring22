243cdedac98868c86cbf3911adf8ef21c81d1d3c
pehaikal <pehaikal@ndu.edu.lb>
2022-06-01 10:56:38 +0200

    Third Commit

--- Tic Tac Toe/js/main.js
+++ Tic Tac Toe/js/main.js
@@ -17,33 +17,58 @@ const b3 = document.querySelector('#b3')
 const c3 = document.querySelector('#c3')
 
 /*
-a1.addEventListener('click', function(){
+a1.addEventListener('click', function () {
     console.log('click on A1')
 })*/
 
-let current=_player = 0
+let current_player = 0
+let winner = false
 const elements = ['X', 'O']
 
+const validationLine = (el1, el2, el3) => {
+    let valid = false
+
+    console.log("valid", el1.innerHTML, el2.innerHTML, el3.innerHTML)
+    
+    if (el1.innerHTML != '' && el2.innerHTML != '' && el3.innerHTML != '') {
+        console.log('valid')
+        valid = true
+    }
+
+    return valid
+}
+
 const cells = document.querySelectorAll('.cell')
-cells.forEach(cell =>{
+cells.forEach(cell => {
     cell.addEventListener('click', (e) => {
         // console.log(e)
-        // e.target.style.background = 'red'
-        console.log(e.target.innerHTML) 
+        console.log(e.target.innerHTML)
         if (e.target.innerHTML == '') {
+            // Put the element of the player in the cell
             e.target.innerHTML = elements[current_player]
 
-            // Switch of palyers
-            if (current_player == 0) {
-                current_player = 1
-            }
-                
-            else {
-                current_player = 0
+            // Check if there is a winner or let the second player plays
+            if (validationLine(a1, a2, a3) ) {
+                console.log('winner !')
+                winner = true
             }
 
+            // show the message if we have a winner
+            if (winner) {
+               let msg = document.querySelector('#msg')
+               msg.innerHTML = 'The winner is Player ' + (current_player+1)
+               msg.style.display = "block"
+            }
+
+            // Switch of players
+            if (current_player == 0) {
+                current_player = 1
+            } else {
+                current_player = 0
+            }
         } else {
-            alert('Someone played in this cell !')
+            // When there is a cliock on a cell already used
+            alert('Someone already play here !')
         }
     })
 })
